b4420d7cd31fa249609a6c07d987f4f22106a58a
Aslbek Kucharov <aslbekkucharov@gmail.com>
2023-10-25 23:11:16 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="wrapper">
+    <section class="section">
+      <div class="flex items-center" style="gap: 14px;">
+        <h2 class="section__title">Список тем обращения и продуктов</h2>
+        <span class="search-result">
+          <span class="search-result__label">Найдено: 3</span>
+        </span>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
